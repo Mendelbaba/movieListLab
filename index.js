da0942c74cd15a62effd793d01eb2960e6e8abf4
Mendelbaba <mendybaba@gmail.com>
2021-10-27 19:10:01 -0400

    think im up to step 5

--- index.js
+++ index.js
@@ -11,13 +11,19 @@ function addMovie(event) {
 
     const movieTitle = document.createElement("span") 
 
-    movieTitle.textContent = inputField.value;
+    movieTitle.textContent = inputField.value
+
+    movieTitle.addEventListener("click",crossOffMovie)
 
     movie.appendChild(movieTitle)
 
-    const deleteButton = document.createElement()
+    const deleteButton = document.createElement("button")
 
     deleteButton.textContent = "x"
+
+    deleteButton.addEventListener("click", deleteMovie)
+
+    movie.appendChild(deleteButton)
 
     const movieList = document.querySelector("ul")
 
@@ -30,3 +36,13 @@ function addMovie(event) {
 const submitButton = document.querySelector("form")
 
 submitButton.addEventListener("submit", addMovie)
+
+deleteButton.addEventListener("click", deleteMovie)
+
+function deleteMovie(event){
+    return event.target.parentNode.remove() 
+}
+
+function crossOffMovie(event) {
+    event.target.classList.toggle(".checked")
+}
