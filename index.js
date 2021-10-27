eba532436f8f98efb2451fba71e90d0a8e36e9c6
Mendelbaba <mendybaba@gmail.com>
2021-10-27 18:45:38 -0400

    first

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("hello world")
+
+
+function addMovie(event) {
+
+    event.preventDefault()
+
+    const inputField = document.querySelector("input")
+
+    const movie = document.createElement("li") 
+
+    const movieTitle = document.createElement("span") 
+
+    movieTitle.textContent = inputField.value;
+
+    movie.appendChild(movieTitle)
+
+    const deleteButton = document.createElement()
+
+    deleteButton.textContent = "x"
+
+    const movieList = document.querySelector("ul")
+
+    movieList.appendChild(movie)
+
+    inputField.value = " "
+
+}
+
+const submitButton = document.querySelector("form")
+
+submitButton.addEventListener("submit", addMovie)
